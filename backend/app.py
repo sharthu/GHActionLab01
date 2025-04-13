4b1c316e1a3bb7db5c8d59212d597155e99d5577
Shar Thu <mr.sharthu@gmail.com>
2025-04-13 10:22:23 +0630

    fixed backend error and all fine

--- backend/app.py
+++ backend/app.py
@@ -9,4 +9,5 @@ def hello():
     return jsonify({'message': 'Hello from the Backend!'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
